0e24aad9bd3f8d7be4fa3741bd3c261c10023112
Benjamin Levin <benjamin.levin883@duke.edu>
2019-02-15 11:42:32 -0500

    Finished

--- src/WordGram.java
+++ src/WordGram.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A WordGram represents a sequence of strings
@@ -14,17 +16,22 @@ public class WordGram {
 
 	/**
 	 * Create WordGram (add comments)
-	 * @param source
-	 * @param start
-	 * @param size
-	 */
+	 * @param source words to be in wordgram
+	 * @param start index of those words
+	 * @param size size of new array
+	 */ 
 	public WordGram(String[] source, int start, int size) {
 		myWords = new String[size];
 		myToString = "";
 		myHash = 5;
 		
 		// TODO: initialize myWords and others as needed
-	}
+		for (int k=0; k<size; k++) {
+			int j = k + start;
+			myWords[k] = source[j];
+		}
+
+		}
 
 	/**
 	 * Return string at specific index in this WordGram
@@ -40,27 +47,41 @@ public class WordGram {
 
 	/**
 	 * Complete this comment
-	 * @return
+	 * calculates length of myWords
+	 * @return length of array
 	 */
 	public int length(){
 		// TODO: change this
-		return 0;
+		
+		return myWords.length;
 	}
 
-
+/**
+ * checks if arrays are equal to each other
+ * returns boolean
+ */ 
 	@Override
 	public boolean equals(Object o) {
 		if (! (o instanceof WordGram) || o == null){
 			return false;
 		}
-
+		
 	    // TODO: complete this method
-		return true;
+		WordGram n = (WordGram) o;
+		if (Arrays.equals(n.myWords, this.myWords)) {
+			return true;
+		}
+		
+		
+		return false;
 	}
 
 	@Override
 	public int hashCode(){
 		// TODO: complete this method
+		if (myHash == 5) {
+		myHash = this.toString().hashCode();
+		}
 		return myHash;
 	}
 	
@@ -68,10 +89,17 @@ public class WordGram {
 	/**
 	 * Create and complete this comment
 	 * @param last is last String of returned WordGram
-	 * @return
+	 * @return wg which is shifted wordgram without first term, last is parameter "last"
 	 */
 	public WordGram shiftAdd(String last) {
-		WordGram wg = new WordGram(myWords,0,myWords.length);
+		ArrayList<String> x = new ArrayList<>();
+		for (int k=0; k<myWords.length-1; k++) {
+			x.add(myWords[k+1]);
+		}
+		x.add(last);
+		String [] convert = new String[myWords.length];
+				x.toArray(convert);
+		WordGram wg = new WordGram(convert,0,myWords.length);
 		// TODO: Complete this method
 		return wg;
 	}
@@ -79,6 +107,9 @@ public class WordGram {
 	@Override
 	public String toString(){
 		// TODO: Complete this method	
+		if (myToString.length() == 0) {
+			myToString = String.join(" ", myWords);
+		}
 		return myToString;
 	}
 }
